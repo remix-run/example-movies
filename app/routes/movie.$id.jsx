d2c2511f3711891d8f80656335bd30a375636347
Michael Hart <michael.hart.au@gmail.com>
2024-03-13 16:12:40 +1100

    Migrate to vite using remix 2.8
    Largely following the guide at https://remix.run/docs/en/main/future/vite#cloudflare

--- app/routes/movie.$id.jsx
+++ app/routes/movie.$id.jsx
@@ -2,7 +2,7 @@ import { useLoaderData } from '@remix-run/react'
 import localforage from 'localforage'
 
 // Initial SSR and first visits will get fresh data from the DB on the server
-export async function loader({ params, context: { env } }) {
+export async function loader({ params, context: { cloudflare: { env } } }) {
   let result = await env.DB.prepare('SELECT * FROM movies WHERE id = ?1')
     .bind(params.id)
     .first()
